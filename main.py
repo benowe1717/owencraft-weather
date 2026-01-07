02705241196d068df1d71072c51514ce5f15bccf
Benjamin Owen <benjamin@projecttiy.com>
2026-01-07 10:31:55 -0500

    added new openweathermap file and supporting functions, added requests to environment and added to requirements.txt, updated main.py to use new openweathermap functions and output the current weather based on the zipcode

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ Minecraft Weather commands, and then set the weather on the target server.
 import sys
 
 from mcrcon.mcrcon import Mcrcon
+from openweathermap import openweathermap
 from parseargs.parseargs import ParseArgs
 
 
@@ -63,12 +64,73 @@ def tasks(parser: ParseArgs) -> None:
     Raises:
         None
     """
-    mcrcon: Mcrcon = Mcrcon()
-    try:
-        mcrcon.set_weather('clear')
-    except ValueError as err:
-        print(err)
+    lat, lon = get_lat_and_lon_from_zipcode(
+        parser.zipcode, parser.country_code)
+
+    if not lat or not lon:
+        print('[ERR] Unable to retrieve latitude and longitude!')
         sys.exit(1)
+
+    weather_id = get_current_weather(lat, lon)
+    current_weather = map_weather_id_to_minecraft_weather(weather_id)
+    print(f'The current weather for {parser.zipcode} is {current_weather}')
+    sys.exit(0)
+
+    # mcrcon: Mcrcon = Mcrcon()
+    # try:
+    #     mcrcon.set_weather('clear')
+    # except ValueError as err:
+    #     print(err)
+    #     sys.exit(1)
+
+
+def get_lat_and_lon_from_zipcode(
+        zipcode: int, country_code: str) -> tuple[str, str]:
+    """
+    Retrieve the latitude and longitude from the given zipcode and
+    country code.
+    """
+    try:
+        lat, lon = openweathermap.get_lat_and_lon(zipcode, country_code)
+        return (lat, lon)
+    except (ValueError, KeyError) as err:
+        print(f'[ERR] {err}')
+        sys.exit(1)
+
+
+def get_current_weather(lat: str, lon: str) -> int:
+    """
+    Get the current weather for the given latitude and longitude.
+    """
+    try:
+        return openweathermap.get_current_weather(lat, lon)
+    except (ValueError, KeyError) as err:
+        print(f'[ERR] {err}')
+        sys.exit(1)
+
+
+def map_weather_id_to_minecraft_weather(weather_id: int) -> str:
+    """
+    Map the Current Weather ID from OpenWeatherMap to Minecraft Weater.
+
+    https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
+    """
+    if weather_id == -1:
+        return 'clear'
+
+    storm: list[int] = [200, 201, 202, 210, 211, 212, 221, 230, 231, 232]
+    rain: list[int] = [
+        300, 301, 302, 310, 311, 312, 313, 314, 321, 500, 501, 502, 503,
+        504, 511, 520, 521, 522, 531, 600, 601, 602, 612, 613, 615, 616,
+        620, 621, 622]
+
+    if weather_id in storm:
+        return 'thunderstorm'
+
+    if weather_id in rain:
+        return 'rain'
+
+    return 'clear'
 
 
 if __name__ == '__main__':
---
+++ openweathermap/openweathermap.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+"""
+OpenWeatherMap API Wrapper
+
+These functions are used to interact with the OpenWeatherMap One Call 3.0
+REST API. Docs can be found here: https://openweathermap.org/api/one-call-3
+"""
+import os
+from typing import Any
+
+import requests
+
+SCHEME: str = 'https'
+DOMAIN: str = 'api.openweathermap.org'
+BASE_URL: str = f'{SCHEME}://{DOMAIN}'
+
+
+def get_api_key() -> str:
+    """
+    Get the API Key from the environment
+    """
+    api_key: str | None = os.getenv('OPENWEATHERMAP_API_KEY')
+    if not api_key:
+        raise ValueError(f'OPENWEATHERMAP_API_KEY is unset!')
+    return api_key
+
+
+def get_lat_and_lon(zipcode: int, country_code: str) -> tuple[str, str]:
+    """
+    Get the Latitude and Longitude from the given zipcode and country code.
+
+    https://openweathermap.org/api/geocoding-api#direct_zip
+    """
+    try:
+        api_key: str = get_api_key()
+    except ValueError as err:
+        raise ValueError(err) from err
+
+    uri: str = f'geo/1.0/zip?zip={zipcode},{country_code}&appid={api_key}'
+    url: str = f'{BASE_URL}/{uri}'
+
+    try:
+        response: requests.Response = requests.get(url=url, timeout=10)
+        if response.status_code != 200:
+            print(f'[ERR] {response.status_code} :: {response.text}')
+            return ('', '')
+
+        data: dict[str, Any] = response.json()
+        lat: str = str(data['lat'])
+        lon: str = str(data['lon'])
+        return (lat, lon)
+    except (
+            requests.exceptions.HTTPError,
+            requests.exceptions.ReadTimeout,
+            requests.exceptions.ConnectionError) as err:
+        raise ValueError(err) from err
+    except KeyError as err:
+        raise KeyError(err) from err
+
+
+def get_current_weather(lat: str, lon: str) -> int:
+    """
+    Get the current weather from the given Latitude and Longitude.
+
+    https://openweathermap.org/api/one-call-3#current
+    """
+    try:
+        api_key: str = get_api_key()
+    except ValueError as err:
+        raise ValueError(err) from err
+
+    parts: list[str] = ['data/3.0/onecall?lat=', lat, '&lon=', lon,
+                        '&exclude=minutely,hourly,daily,alerts&appid=', api_key]
+    uri: str = ''.join(parts)
+    url: str = f'{BASE_URL}/{uri}'
+
+    try:
+        response: requests.Response = requests.get(url=url, timeout=10)
+        if response.status_code != 200:
+            print(f'[ERR] {response.status_code} :: {response.text}')
+            return -1
+
+        data: dict[str, Any] = response.json()
+        return data['current']['weather'][0]['id']
+    except (
+            requests.exceptions.HTTPError,
+            requests.exceptions.ReadTimeout,
+            requests.exceptions.ConnectionError) as err:
+        raise ValueError(err) from err
+    except KeyError as err:
+        raise KeyError(err) from err
